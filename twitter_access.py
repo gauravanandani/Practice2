f03ed56424046301410c95c6609a1ee8df25fdea
root <root@localhost.localdomain>
2019-01-24 13:40:01 +0530

    hadoop24

--- twitter_access.py
+++ twitter_access.py
@@ -3,13 +3,13 @@
 import  tweepy
 import  sys
 topic=sys.argv[1]
-consumer_key=''
-consumer_sec=''
+consumer_key='iifQfMm9PARUNfHoMHmXcl8qV'
+consumer_sec='qTFR4SCp9PhYLpd2SOMhqSvm2KlQ9KeraOBcc50FwthoKV69m0'
 #  by using above keys  we are going to check auth handler 
 auth=tweepy.OAuthHandler(consumer_key,consumer_sec)
 #  here auth is token by consuker key and sec 
-access_key=''
-secret_key=''
+access_key='2444201649-VoPPCk8aPzHwQgU6gskEqLEU8iIZkKiKsizw12H'
+secret_key='O1RqNilViFvh02FLdeTdSmcLGPjHB8A2IuY5mcH3o9qwp'
 #  connecting  data server  with  access and secret key  by using above token
 print(dir(auth))
 auth.set_access_token(access_key,secret_key)
